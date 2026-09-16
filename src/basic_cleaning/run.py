916a07891adf031947ec1f633a750b2c42f7c951
Eric Freeman <freeman.e.l@gmail.com>
2026-09-15 22:42:28 -0700

    Update root MLproject with hydra_options and add NYC boundary filter for sample2.csv

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -7,18 +7,17 @@ import logging
 import wandb
 import pandas as pd
 
-
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
 logger = logging.getLogger()
 
+
 # DO NOT MODIFY
 def go(args):
-
     run = wandb.init(job_type="basic_cleaning")
     run.config.update(args)
 
     # Download input artifact. This will also log that this script is using this
-    
+
     run = wandb.init(project="nyc_airbnb", group="cleaning", save_code=True)
     # artifact_local_path = run.use_artifact(args.input_artifact).file()
     artifact_local_path = run.use_artifact(args.input_artifact).file(root="artifacts/sample_latest")
@@ -32,13 +31,12 @@ def go(args):
     df['last_review'] = pd.to_datetime(df['last_review'])
 
     # Step 6: TODO
-    # Only implement this step when reaching Step 6: Pipeline Release and Updates
-    # in the project.
     # Add longitude and latitude filter to allow test_proper_boundaries to pass
-    # ENTER CODE HERE
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
 
     # Save the cleaned data
-    df.to_csv('clean_sample.csv',index=False)
+    df.to_csv('clean_sample.csv', index=False)
 
     # log the new data.
     artifact = wandb.Artifact(
@@ -50,42 +48,39 @@ def go(args):
     run.log_artifact(artifact)
 
 
-# TODO: In the code below, fill in the data type for each argument. The data type should be str, float or int. 
-# TODO: In the code below, fill in a description for each argument. The description should be a string.
 if __name__ == "__main__":
-
     parser = argparse.ArgumentParser(description="A very basic data cleaning")
-    
+
     parser.add_argument(
-        "--input_artifact", 
+        "--input_artifact",
         type=str,
         help="Initial artifact to be cleaned",
         required=True
     )
 
     parser.add_argument(
-        "--output_artifact", 
+        "--output_artifact",
         type=str,
         help="Output artifact for cleaned data",
         required=True
     )
 
     parser.add_argument(
-        "--output_type", 
+        "--output_type",
         type=str,
         help="Type of the output dataset",
         required=True
     )
 
     parser.add_argument(
-        "--output_description", 
+        "--output_description",
         type=str,
         help="Description of the output dataset",
         required=True
     )
 
     parser.add_argument(
-        "--min_price", 
+        "--min_price",
         type=float,
         help="Minimum house price to be considered",
         required=True
@@ -98,8 +93,6 @@ if __name__ == "__main__":
         required=True
     )
 
-
     args = parser.parse_args()
 
     go(args)
-    
